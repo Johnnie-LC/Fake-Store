9ce52d063abd571279783b02a7b2b6fc6161f7a4
Johnnie Lopez <johesloc@gmail.com>
2021-08-13 22:34:36 -0500

    added /Fake-store/ in Header container

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className="ui fixed menu">
-      <div className="ui container center">
-        <h2>FakeShop</h2>
+    <Link to="/Fake-Store/">
+      <div className="ui fixed menu">
+        <div className="ui container center">
+          <Link to="/Fake-Store/">
+            <h2>FakeShop</h2>
+          </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 export default Header
